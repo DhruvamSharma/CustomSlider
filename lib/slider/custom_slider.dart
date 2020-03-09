d62a39778f82a5f39ef1bfdfab77aeed13ff49f8
DhruvamSharma <dhruvamssharma@gmail.com>
2020-03-09 13:31:50 +0530

    feat(slider setup): slider class changed from stateful to stateless using provider.

--- lib/slider/custom_slider.dart
+++ lib/slider/custom_slider.dart
@@ -16,23 +16,24 @@ class CustomSlider extends StatelessWidget {
         valueIndicatorColor: CommonColors.valueIndicatorColor,
         showValueIndicator: ShowValueIndicator.onlyForContinuous,
         valueIndicatorTextStyle:
-        CommonTextStyles.getMediumTextStyleLight(context).copyWith(
+            CommonTextStyles.getMediumTextStyleLight(context).copyWith(
           fontSize: 10,
         ),
         thumbColor: CommonColors.grayColor,
       ),
       child: Slider(
-          label: '${Provider.of<SliderValueNotifier>(context).currentValue.round()} players',
+          label:
+              '${Provider.of<SliderValueNotifier>(context).currentValue.round()} players',
           min: 0,
           max: 100,
           value: Provider.of<SliderValueNotifier>(context).currentValue,
           onChanged: (value) {
-            Provider.of<SliderValueNotifier>(context, listen: false).changeValue(value);
+            Provider.of<SliderValueNotifier>(context, listen: false)
+                .changeValue(value);
           }),
     );
   }
 }
-
 
 class SliderValueNotifier extends ChangeNotifier {
   double _currentValue = 20;
@@ -40,5 +41,6 @@ class SliderValueNotifier extends ChangeNotifier {
     _currentValue = value;
     notifyListeners();
   }
+
   double get currentValue => _currentValue;
 }
